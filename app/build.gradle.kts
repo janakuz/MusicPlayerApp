e8008c722ba1499bfe8da772bb7a3776d2fc2e60
janakuz <kuzmanovajana@gmail.com>
2026-01-15 23:19:57 +0100

    fix Android 16 playback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,12 +17,12 @@ apikeyProperties.load(FileInputStream(apikeyPropertiesFile))
 
 android {
     namespace = "com.example.musicapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.musicapp"
-        minSdk = 24
-        targetSdk = 35
+        minSdk = 33
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.musicapp
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import com.example.musicapp.ui.theme.MusicAppTheme
 import com.example.musicapp.ui.viewmodels.PlayerViewModel
@@ -14,9 +16,24 @@ class MainActivity : ComponentActivity() {
 //    private lateinit var mediaController: MediaController
     private val playerViewModel: PlayerViewModel by viewModels()
 
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        if (permissions[Manifest.permission.READ_MEDIA_AUDIO] == true &&
+            permissions[Manifest.permission.READ_MEDIA_IMAGES] == true) {
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DevLibraryBootstrap.ensureSampleTracksAvailable(this)
+
+        val permissionsToRequest = arrayOf(
+            Manifest.permission.READ_MEDIA_AUDIO,
+            Manifest.permission.READ_MEDIA_IMAGES
+        )
+        requestPermissionLauncher.launch(permissionsToRequest)
 
 //        val sessionToken = SessionToken(this, ComponentName(this, PlaybackService::class.java))
 //        val controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
