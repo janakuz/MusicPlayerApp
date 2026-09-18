54857982ec7fbf31d595439359b1148b9794bfd7
janakuz <kuzmanovajana@gmail.com>
2026-09-18 20:26:33 +0200

    update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.musicapp"
         minSdk = 33
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.3.1"
+        versionCode = 7
+        versionName = "1.3.2"
 
         android.buildFeatures.buildConfig = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
